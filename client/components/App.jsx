6378e84385fcbce6bfa2bae03d837e8461b34938
EDA <eda@mac-mini-5.local>
2017-05-26 09:37:50 +1200

    Started App component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+// import {HashRouter as Router, Route} from 'react-router-dom'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+import {Quote} from './Quote'
+
+export class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render () {
+    return (
+      <div className='main'>
+        <h1>This is a header</h1>
+        <Quote quote={'the quote goes here'} />
+      </div>
+    )
+  }
 }
-
-export default App
-
