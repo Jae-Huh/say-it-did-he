eff93c87b347665743d6a79e4fe3cb4b0f15ba45
Rob <superserva@gmail.com>
2017-05-26 13:13:36 +1200

    passing getYoda function test

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,9 @@
 import test from 'tape'
 import React from 'react'
+import request from 'supertest'
+
 import { shallow } from 'enzyme'
+import { getYoda } from '../client/api/yodaAPI.js'
 
 import App from '../client/components/App'
 
@@ -9,4 +12,16 @@ test('<App />', t => {
   const wrapper = shallow(<App />)
   t.equal(wrapper.text(), expected)
   t.end()
+})
+
+
+test("test getYoda function", function (t) {
+  // Arrange
+  // Act
+  getYoda('It was the best of times.', (err,result) => {
+        
+    // Assert
+    t.is(typeof result, 'string')
+    t.end()
+  })
 })
