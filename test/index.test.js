a1ad2509b8bf23eb3f5839621c393f82d84d7a58
EDA <pbrabet@gmail.com>
2017-05-26 13:49:19 +1200

    tests for v1 api complete

--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,40 @@
 import test from 'tape'
 import React from 'react'
 import { shallow } from 'enzyme'
+import request from 'superagent'
 
-import App from '../client/components/App'
+import { App } from '../client/components/App.jsx'
+import getQuote from '../client/api/getQuote.js'
 
-test('<App />', t => {
-  const expected = 'React development has begun!'
+test('Checks if title is in place', t => {
   const wrapper = shallow(<App />)
-  t.equal(wrapper.text(), expected)
+  t.is(wrapper.find('h1').text(), 'Say it, did he?')
   t.end()
+})
+
+test('getQuote returns an object', function (t) {
+  getQuote((err, result) => {
+    t.is(typeof result, 'object')
+    t.end()
+  })
+})
+
+test('getQuote returns a string for quote key in an object', function (t) {
+  getQuote((err, result) => {
+    t.is(typeof result.quote, 'string')
+    t.end()
+  })
+})
+
+test('getQuote returns a string for author key in an object', function (t) {
+  getQuote((err, result) => {
+    t.is(typeof result.author, 'string')
+    t.end()
+  })
+})
+test('getQuote returns a string for category key in an object', function (t) {
+  getQuote((err, result) => {
+    t.is(typeof result.category, 'string')
+    t.end()
+  })
 })
